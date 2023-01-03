87955d077cd4c1f2b68c179ffacb0017c1e205f4
Leonardo <cioni.leonardo@gmail.com>
2023-01-03 19:37:56 +0100

    remove exit node

--- LantoLan/entrypoint.sh
+++ LantoLan/entrypoint.sh
@@ -8,5 +8,5 @@ if [ ! -d /dev/net ]; then mkdir /dev/net; fi
 if [ ! -e /dev/net/tun ]; then  mknod /dev/net/tun c 10 200; fi
 
 # Wait 5s for the daemon to start and then run tailscale up to configure
-/bin/sh -c "sleep 5; tailscale up --advertise-routes ${ROUTES} --snat-subnet-routes=false --login-server ${LOGINSERVER} --authkey ${AUTHKEY} --advertise-exit-node --accept-routes" &
+/bin/sh -c "sleep 5; tailscale up --advertise-routes ${ROUTES} --snat-subnet-routes=false --login-server ${LOGINSERVER} --authkey ${AUTHKEY} --accept-routes" &
 exec /usr/bin/tailscaled --state=/tailscale/tailscaled.state
